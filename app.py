709b28f9d64deb5d61a093054c43f4ea0d139f36
Alexander Guevara <Alexander.guevaram@hotmail.com>
2026-03-16 22:27:29 -0500

    fix: Siempre en Light

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+
 # ── Estilos ──────────────────────────────────────────────────────
 with open("styles/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
